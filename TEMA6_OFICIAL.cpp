82d6a17099cf998ad19efaba5553b1f448010191
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2020-03-17 23:24:22 +0200

    Update TEMA6_OFICIAL.cpp

--- TEMA6_OFICIAL.cpp
+++ TEMA6_OFICIAL.cpp
@@ -1,37 +1,51 @@
 #include<iostream>
 using namespace std;
 
-class vector{
+class vector
+{
     int nv;
     int *v;
-    public:
+public:
+    friend class vectori_de_vectori;
+    vector()        ///constructori de initializare
+    {
+        nv = 0;
+        v = NULL;
+    }
+    vector(int n, int *w)
+    {
+        nv=n;
+        v=new int[nv+1];
+        for(int i=1; i<=nv; i++)
+            v[i]=w[i];
+    }
 
-        vector() {      ///constructor de initializare
-            nv = 0;
-            v = NULL;
-        }
-        vector(int n, int *w)
-        {
-            nv=n;
-            v=w;
-        }
-        vector(vector &ob){  ///constructor de copiere
-            nv=ob.nv;
-            for(int i=1;i<=nv;i++)
-                v[i]=ob.v[i];
-        }
-        ~vector()    ///destructor
-        {
-            delete &nv;
-            delete &v;
-        }
+    vector(int n)   ///initializare cu 0
+    {
+        nv=n;
+        v=new int[nv+1];
+        for(int i=1; i<=nv; i++)
+            v[i]=0;
+    }
 
-        friend vector operator + (vector &ob1,vector &ob2);
-        friend class vectori_de_vectori;
-        friend istream &operator >> (istream &i, vector &ob);
-        friend ostream &operator << (ostream &o, vector &ob);
-        public: int lungime_de_vector();
-        public: int* vector_simplu(int l);
+    vector(vector &ob)   ///constructor de copiere
+    {
+        nv=ob.nv;
+        v=new int[nv+1];
+        for(int i=1; i<=nv; i++)
+            v[i]=ob.v[i];
+    }
+    ~vector()    ///destructor
+    {
+        nv=0;
+        delete[] v;
+    }
+
+    friend vector operator + (vector &ob1,vector &ob2);
+    friend istream &operator >> (istream &i, vector &ob);
+    friend ostream &operator << (ostream &o, vector &ob);
+    int lungime();
+    int* vector_simplu(int l);
 };
 
 istream &operator >> ( istream &i,vector &ob)   /// >>
@@ -40,36 +54,30 @@ istream &operator >> ( istream &i,vector &ob)   /// >>
     i>>ob.nv;
     ob.v=new int[ob.nv+2];
     cout<<"cele m componente:";
-    for(int j=1;j<=ob.nv;j++)
+    for(int j=1; j<=ob.nv; j++)
         i>>ob.v[j];
     return i;
 }
 
 ostream& operator << (ostream &o, vector &ob)  /// <<
 {
-   // o<<ob.nv<<'\n';
-    for(int i=1;i<=ob.nv;i++)
+    // o<<ob.nv<<'\n';
+    for(int i=1; i<=ob.nv; i++)
         o<<ob.v[i]<<" ";
     return o;
 }
 
- vector operator + (vector &ob1,vector &ob2)     /// + imi returneaza tot un obiect.
+vector operator + (vector &ob1,vector &ob2)     /// + imi returneaza tot un obiect.
 {
-    vector ob3;
-    ob3.nv=max(ob1.nv,ob2.nv);
-    ob3.v=new int[ob3.nv+1];
-    for(int i=1;i<=ob3.nv;i++)
-    {
-        ob3.v[i]=0;
-        if(ob1.nv>=i)
-            ob3.v[i]+=ob1.v[i];
-        if(ob2.nv>=i)
-            ob3.v[i]+=ob2.v[i];
-    }
+    vector ob3(max(ob1.nv,ob2.nv));
+    for(int i = 1; i <= ob1.nv; i++)
+        ob3.v[i] = ob1.v[i];
+    for(int i = 1; i <= ob2.nv; i++)
+        ob3.v[i] += ob2.v[i];
     return ob3;
 }
 
-int vector::lungime_de_vector()
+int vector::lungime()
 {
     return this->nv;
 }
@@ -78,7 +86,7 @@ int* vector::vector_simplu(int l)/// imi returneaza doar vectorul din obiect, ad
 {
     int *w;
     w=new int[l+1];
-    for(int i=1;i<=l;i++)
+    for(int i=1; i<=l; i++)
     {
         if(i<=this->nv)
             w[i]=this->v[i];
@@ -89,17 +97,11 @@ int* vector::vector_simplu(int l)/// imi returneaza doar vectorul din obiect, ad
 }
 
 
-
-
-
-
-
-
 class vectori_de_vectori
 {
     int dim;
     vector *m;
-    public:
+public:
 
     //supraincarcarea operatorilor:
 
@@ -115,41 +117,33 @@ class vectori_de_vectori
     vectori_de_vectori(int n,int *c,int nr); ///initializare cu un nr pe n linii
     vectori_de_vectori(const vectori_de_vectori &ob);///copiere
     ~vectori_de_vectori(); ///destructor
-    public: int** matrice();
+    int** matrice();
     int i_max(vectori_de_vectori &ob2);
     int j_max(vectori_de_vectori &ob2);
+    int j_max();
 
 };
-
-
 
 vectori_de_vectori::vectori_de_vectori(int n,int *c,int nr)            ///constructor de initializare cu un nr pe n linii
 {
 
     this->dim=n;
     this->m=new vector[dim+1];
-    cout<<n<<" "<<this->dim<<endl;
-    for(int i=1;i<=this->dim;i++)
+    for(int i=1; i<=this->dim; i++)
     {
         m[i].v=new int[c[i]+1];
         this->m[i].nv=c[i];
+        for(int j=1; j<=c[i]; j++)
+            m[i].v[j]=nr;
 
-
-        cout<<c[i]<<": ";           //verificare
-        for(int j=1;j<=c[i];j++)
-            {m[i].v[j]=nr;
-            cout<<m[i].v[j]<<" ";
-            }
-        cout<<endl;
     }
-    cout<<endl;
-
 }
 
 vectori_de_vectori::vectori_de_vectori(const vectori_de_vectori &ob)   ///constructor de copiere
 {
     dim=ob.dim;
-    for(int i=1;i<=ob.dim;i++)
+    m=new vector[dim+1];
+    for(int i=1; i<=ob.dim; i++)
         m[i]=ob.m[i];
 }
 
@@ -158,24 +152,23 @@ istream &operator >> (istream &i,  vectori_de_vectori &ob)             ///>>
     cout<<"n=";
     i>>ob.dim;
     ob.m=new vector[ob.dim+1];
-    for(int j=1;j<=ob.dim;j++)
-        {
+    for(int j=1; j<=ob.dim; j++)
+    {
         i>>ob.m[j];
-        }
+    }
     return i;
 }
 
 ostream &operator << (ostream &o, vectori_de_vectori &ob)              ///<<
 {
-   // cout<<ob.dim;
-    for(int i=1;i<=ob.dim;i++)
+    o<<ob.dim<<'\n';
+    for(int i=1; i<=ob.dim; i++)
     {
-       // cout<<ob.m[i]<<" ";
+        // cout<<ob.m[i]<<" ";
         o<<ob.m[i]<<'\n';
     }
     return o;
 }
-
 
 int vectori_de_vectori::i_max(vectori_de_vectori &ob2)  ///i_max intre doua matrice
 {
@@ -184,29 +177,31 @@ int vectori_de_vectori::i_max(vectori_de_vectori &ob2)  ///i_max intre doua matr
     return ob2.dim;
 }
 
-int vectori_de_vectori::j_max(vectori_de_vectori &ob2)  ///j_max dintre toate liniile din prima si din a doua matrice
+int vectori_de_vectori::j_max()
 {
     int max=0;
-    for(int i=1;i<=this->dim;i++)
-        if(this->m[i].lungime_de_vector()>max)
-            max=this->m[i].lungime_de_vector();
-      for(int i=1;i<=ob2.dim;i++)
-        if(ob2.m[i].lungime_de_vector()>max)
-            max=ob2.m[i].lungime_de_vector();
+    for(int i=1; i<=this->dim; i++)
+        if(this->m[i].lungime()>max)
+            max=this->m[i].lungime();
     return max;
 }
 
+int vectori_de_vectori::j_max(vectori_de_vectori &ob2)  ///j_max dintre toate liniile din prima si din a doua matrice
+{
+    if(this->j_max()>ob2.j_max())
+        return this->j_max();
+    return ob2.j_max();
+}
 
 int** vectori_de_vectori::matrice()         ///transform un vector de vectori in matrice
 {
     int** a=new int*[this->dim+1];
     int jmax=this->j_max(*this);
-    for(int i=1;i<=dim;i++)
+    for(int i=1; i<=dim; i++)
     {
-        a[i]=new int[jmax+1];
         a[i]=this->m[i].vector_simplu(jmax);
 
-        for(int j=1;j<=jmax;j++)    //verificare
+        for(int j=1; j<=jmax; j++)  //verificare
             cout<<a[i][j]<<" ";
         cout<<'\n';
     }
@@ -219,16 +214,16 @@ int** &operator + (vectori_de_vectori &ob1,vectori_de_vectori &ob2)   ///adunare
     int jmax=ob1.j_max(ob2);
     cout<<imax<<" "<<jmax<<endl;
     int** a=new int*[imax+1];
-    for(int i=1;i<=imax;i++)
+    for(int i=1; i<=imax; i++)
     {
-        a[i]=new int[jmax+1];
         if(ob1.dim<i)
             a[i]=(ob2.m[i]).vector_simplu(jmax);
         else if(ob2.dim<i)
-                a[i]=(ob1.m[i]).vector_simplu(jmax);
+            a[i]=(ob1.m[i]).vector_simplu(jmax);
         else
             a[i]=(ob1.m[i]+ob2.m[i]).vector_simplu(jmax);
-        for(int j=1;j<=jmax;j++) //verificare
+
+        for(int j=1; j<=jmax; j++) //verificare
             cout<<a[i][j]<<" ";
         cout<<endl;
     }
@@ -239,47 +234,37 @@ int** &operator + (vectori_de_vectori &ob1,vectori_de_vectori &ob2)   ///adunare
 
 vectori_de_vectori::~vectori_de_vectori()                              ///destructor
 {
-    for(int i=1;i<=dim;i++)
-        delete &m[i];
-    delete &dim;
+    dim=0;
+    delete[] m;
 }
 
 int main()
 {
 
-///1.CITIRE (SUPRAINCARCAREA LUI >>)
-vectori_de_vectori ob2;
+///1.citire (supraincarcarea lui >>)
+    vectori_de_vectori ob2;
     cout<<'\n'<<'\n'<<"CITIRE:"<<'\n';
     cin>>ob2; //se vor citi: n=numarul de linii (numarul de vectori din vector)
-                            //apoi de n ori cate un m si apoi cele m numere pentru linia i. (1<=i<=n)
-
+    //apoi de n ori cate un m si apoi cele m numere pentru linia i. (1<=i<=n)
     cout<<'\n'<<"Obiectul citit este:"<<'\n'<<ob2<<'\n'<<'\n';//verificare
 
-
-
-///2.DECLARARE:
-    int v[8]={0,1,2,3,4,5,6,7};//v[i]=numarul de componente pentru al i-lea vector din matrice
+///2.declarare:
+    int v[8]= {0,1,2,3,4,5,6,7}; //v[i]=numarul de componente pentru al i-lea vector din matrice
     cout<<"DECLARARE:"<<endl;
-    vectori_de_vectori ob1(5,v,3);//am lasat afisari in initializare pt verificare
+    vectori_de_vectori ob1(5,v,3);
+    cout<<ob1;
 
-
-
-///3.AFISARE (SUPRAINCARCAREA LUI << )
+///3.afisare (supraincarcarea lui << )
     cout<<'\n'<<"AFISARE:"<<'\n'<<ob2<<'\n'<<'\n'<<ob1;//se afiseaza doar ob1.m si ob2.m, adica vectorii de vectori, nu si dimensiunile lor
 
-
-
-///4.METODA DE TRANSFORMARE IN MATRICE
+///4.metoda de transformare in matrice
     cout<<'\n'<<"TRANSFORMAREA OBIECTULUI IN MATRICE:"<<endl;
     int **a=ob2.matrice(); //am pus afisarea in metoda, pentru verificare
 
-
-
-///5.METODA DE ADUNARE A DOUA OBIECTE SI FURNIZAREA REZULTATULUI PRINTR-O MATRICE NORMALA
-    cout<<'\n'<<"ADUNAREA CELOR DOUA MATRICI/E:"<<endl;
+///5.metoda de adunare a doua obiecte
+    cout<<'\n'<<"ADUNAREA CELOR DOUA MATRICE:"<<endl;
     int **b=ob1+ob2;
 
-
-return 0;
+    return 0;
 
 }
